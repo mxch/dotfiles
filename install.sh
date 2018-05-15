f896996951a69b8e1ec46e887b72573a3ba13bd0
mxch <mxch@users.noreply.github.com>
2018-05-14 23:19:02 -0700

    install pass and notmuch

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DOTFILES_DIR="$HOME/.dotfiles"
 SETUP_DIR="${DOTFILES_DIR}/setup"
 CURR_DIR=$(pwd)
 DOTFILES=(tmux git vim zsh dir_colors mbsync mutt notmuch scripts)
-APPS=(tmux vim zsh stow tree curl mutt isync)
+APPS=(tmux vim zsh stow tree curl mutt isync notmuch pass)
 
 # install apps
 for app in ${APPS[@]}; do
